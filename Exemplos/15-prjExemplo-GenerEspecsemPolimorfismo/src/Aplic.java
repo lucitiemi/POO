e024921e1ffb5f9fab0b195f7d7454ae082fc3a0
Luciana <lt.shimoyama@gmail.com>
2025-03-11 17:38:23 -0300

    Implementação classe Professor - prjExemplo 15

--- Exemplos/15-prjExemplo-GenerEspecsemPolimorfismo/src/Aplic.java
+++ Exemplos/15-prjExemplo-GenerEspecsemPolimorfismo/src/Aplic.java
@@ -1,5 +1,6 @@
 
 import fatec.poo.model.Aluno;
+import fatec.poo.model.Professor;
 
 /**
  *
@@ -7,16 +8,29 @@ import fatec.poo.model.Aluno;
  */
 public class Aplic {
     public static void main(String[] args) {       
-       //Instanciação de um objeto da classe Aluno
-       Aluno objAlu = new Aluno(1010,
+        //Instanciação de um objeto da classe Aluno
+        Aluno objAlu = new Aluno(1010,
                                 "Carlos Silveira", 
                                 "15/03/1978");
     
-      //passagem de mensagens   
-      objAlu.setMensalidade(1500);
-      System.out.println("Registro escolar: " + objAlu.getRegEscolar());
-      System.out.println("Nome: " + objAlu.getNome());
-      System.out.println("Data de Nascimento: " + objAlu.getDataNascimento());
-      System.out.println("Mensalidade: " + objAlu.getMensalidade()); 
+        //passagem de mensagens   
+        objAlu.setMensalidade(1500);
+        System.out.println("-DADOS ALUNO-");
+        System.out.println("Registro escolar: " + objAlu.getRegEscolar());
+        System.out.println("Nome: " + objAlu.getNome());
+        System.out.println("Data de Nascimento: " + objAlu.getDataNascimento());
+        System.out.println("Mensalidade: " + objAlu.getMensalidade()); 
+
+
+        //Instanciação de um objeto da classe Professor
+        Professor objProf = new Professor(1001, "Dimas", "05/09/1981");
+
+        objProf.setSalario(10000);
+        System.out.println("\n-DADOS PROFESSOR-");
+        System.out.println("Registro Funcional: " + objProf.getRegFuncional());
+        System.out.println("Nome: " + objProf.getNome());
+        System.out.println("Data de Nascimento: " + objProf.getDataNascimento());
+        System.out.println("Salário: " + objProf.getSalario());
+      
     }
 }
---
+++ Exemplos/15-prjExemplo-GenerEspecsemPolimorfismo/src/fatec/poo/model/Professor.java
@@ -0,0 +1,28 @@
+package fatec.poo.model;
+
+/**
+ *
+ * @author Luciana
+ * 
+ */   
+
+public class Professor extends Pessoa{ 
+    private int regFuncional;
+    private double salario;
+
+    public Professor(int rf, String n, String dn) {
+        super(n, dn);
+        regFuncional = rf;
+    }
+
+    public void setSalario(double s) {
+        salario = s;
+    }
+    public int getRegFuncional() {
+        return regFuncional;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+}
