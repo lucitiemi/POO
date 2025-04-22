f3b61e4a3489bbe9996518c5e7bf973c47965307
Luciana <lt.shimoyama@gmail.com>
2025-04-22 10:58:34 -0300

    Simplificação da Associação Binária com o This

--- Exemplos/23-prjExemplo-ArrayListThis/src/Aplic.java
+++ Exemplos/23-prjExemplo-ArrayListThis/src/Aplic.java
@@ -40,73 +40,42 @@ public class Aplic {
         Projeto objPrj = new Projeto(1234, "Segurança no Trabalho");
         objPrj.setDtInicio("02/05/2024");
         objPrj.setDtTermino("25/05/2024");  
+        
+        objDep1.addFuncionario(funcHor);
+        objDep1.addFuncionario(funcMen);
+        objDep2.addFuncionario(funcCom);
        
-       //Estabelecendo a associação binária entre 1
-       //objeto da classe FuncionarioHorista com
-       //1 objeto da classe Departamento
-       funcHor.setDepartamento(objDep1);
+      
        System.out.println("O funcionário horista " + funcHor.getNome() +
-                          " trabalha no deparamento " + funcHor.getDepartamento().getNome());
+                          " trabalha no departamento " + funcHor.getDepartamento().getNome());
        
-       
-       //Estabelecendo a associação binária entre 1
-       //objeto da classe FuncionarioMensalista com
-       //1 objeto da classe Departamento
-       funcMen.setDepartamento(objDep1);
        System.out.println("O funcionário mensalista " + funcMen.getNome() +
                            " trabalha no departamento " + funcMen.getDepartamento().getNome());
        
-      
-       //Estabelecendo a associação binária entre 1
-       //objeto da classe FuncionarioComissionado com
-       //1 objeto da classe Departamento
-       funcCom.setDepartamento(objDep2);
        System.out.println("O funcionário comissionado " + funcCom.getNome() +
                            " trabalha no departamento " +  funcCom.getDepartamento().getNome());
-      
-       //Estabelecendo a associação binária entre um
-       //objeto da classe Departamento com 1 ou mais (1..*)
-       //objetos da classe FuncionarioHorista, FuncionarioMensalista
-       //e FuncionarioComissionado  
-       objDep1.addFuncionario(funcHor);
-       objDep1.addFuncionario(funcMen);
-       objDep2.addFuncionario(funcCom);
        
        objDep1.listarFuncionarios();
        objDep2.listarFuncionarios();
        
-       //estabelecendo a associação binária
-       //entre 1 objeto da classe FuncionarioHorista
-       //com um objeto da classe Projeto
-       funcHor.setProjeto(objPrj);
+       
+       objPrj.addFuncionario(funcHor);
+       objPrj.addFuncionario(funcMen);
+       objPrj.addFuncionario(funcCom);  
+       
        System.out.println("\n\nO funcionário horista " + funcHor.getNome() +
                            " está alocado no projeto " + 
                            funcHor.getProjeto().getDescricao());
        
-       //estabelecendo a associação binária
-       //entre 1 objeto da classe FuncionarioMensalista
-       //com um objeto da classe Projeto
-       funcMen.setProjeto(objPrj);
        System.out.println("O funcionário mensalista " + funcMen.getNome() +
                            " está alocado no projeto " + 
                            funcMen.getProjeto().getDescricao());
 
-       //estabelecendo a associação binária
-       //entre 1 objeto da classe FuncionarioComissionado
-       //com 1 objeto da classe Projeto
        funcCom.setProjeto(objPrj);
        System.out.println("O funcionário comissionado " + funcCom.getNome() +
                            " está alocado no projeto " + 
                            funcCom.getProjeto().getDescricao());
        
-       //estabelecendo a associação binária
-       //entre 1 objeto da classe Projeto
-       //com 1 ou mais objetos da classe Funcionario 
-       //(Horista, Mensalista, Comissionado)
-       objPrj.addFuncionario(funcHor);
-       objPrj.addFuncionario(funcMen);
-       objPrj.addFuncionario(funcCom);    
-
        objPrj.listarFuncionarios();
     }    
 }
